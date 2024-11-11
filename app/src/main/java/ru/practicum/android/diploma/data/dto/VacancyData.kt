bd0e1531f4c0a4616c39e02fcf28f57a573459bc
Corpseofpig <Corpseofpig@mail.ru>
2024-11-11 15:34:39 +0300

    Созданы модели данных

---
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/VacancyData.kt
@@ -0,0 +1,66 @@
+package ru.practicum.android.diploma.data.dto
+import com.google.gson.annotations.SerializedName
+import ru.practicum.android.diploma.domain.api.DriverLicense
+import ru.practicum.android.diploma.domain.api.Employer
+import ru.practicum.android.diploma.domain.api.Employment
+import ru.practicum.android.diploma.domain.api.Experience
+import ru.practicum.android.diploma.domain.api.KeySkill
+import ru.practicum.android.diploma.domain.api.Language
+import ru.practicum.android.diploma.domain.api.Salary
+
+data class VacancyData(
+    //Для JobItem
+    val name: String, //название вакансии
+    val employer: Employer, //работодатель
+    val salary: Salary, //зарплата
+
+    //Для Описания вакансии
+    val experience: Experience, //опыт
+    val employment: Employment, //тип занятости
+    val key_skills: List<KeySkill>?, //ключевые навыки
+    val languages: List<Language>?, //языки
+    val driver_license_types: List<DriverLicense> //категория прав водителя
+)
+
+data class Employer(
+    val name: String, //название работодателя
+    val logo_urls: LogoUrls // Иконки работодателя
+)
+
+data class Salary(
+    val currency: String?, //валюта
+    val from: Int?, //сумма от
+    val gross: Boolean?, // до или после вычета
+    val to: Int? //сумма до
+)
+
+data class LogoUrls(
+    @SerializedName("90") val small: String, //Маленькая иконка
+    @SerializedName("240") val medium: String, //Большая иконка
+    val original: String //стандартная иконка
+)
+
+data class Experience(
+    val name: String // Строка "требуемый опыт"
+)
+
+data class Employment(
+    val name: String // строка "тип занятости"
+)
+
+data class KeySkill(
+     val name: String // Описание(Обязанности)
+)
+
+data class Language(
+    val name: String, // Название языка
+    val level: LanguageLevel
+)
+
+data class LanguageLevel(
+    val name: String //Уровень языка
+)
+
+data class DriverLicense(
+    val id: String
+)
