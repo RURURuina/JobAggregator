f733fca61b2f41eaf476ca9293b8fb93bd9d3e58
Corpseofpig <Corpseofpig@mail.ru>
2024-11-11 18:23:10 +0300

    Созданы модели данных. Переписал комы

--- app/src/main/java/ru/practicum/android/diploma/domain/api/VacancyDomain.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/VacancyDomain.kt
@@ -2,34 +2,34 @@ package ru.practicum.android.diploma.domain.api
 import com.google.gson.annotations.SerializedName
 data class VacancyDomain(
     //Для JobItem
-    val name: String, //название вакансии
-    val employer: Employer, //работодатель
-    val salary: Salary, //зарплата
+    val name: String, // название вакансии
+    val employer: Employer, // работодатель
+    val salary: Salary, // зарплата
 
     //Для Описания вакансии
-    val experience: Experience, //опыт
-    val employment: Employment, //тип занятости
-    val key_skills: List<KeySkill>?, //ключевые навыки
-    val languages: List<Language>?, //языки
-    val driver_license_types: List<DriverLicense> //категория прав водителя
+    val experience: Experience, // опыт
+    val employment: Employment, // тип занятости
+    val key_skills: List<KeySkill>?, // ключевые навыки
+    val languages: List<Language>?, // языки
+    val driver_license_types: List<DriverLicense> // категория прав водителя
 )
 
 data class Employer(
-    val name: String, //название работодателя
+    val name: String, // название работодателя
     val logo_urls: LogoUrls // Иконки работодателя
 )
 
 data class Salary(
-    val currency: String?, //валюта
-    val from: Int?, //сумма от
+    val currency: String?, // валюта
+    val from: Int?, // сумма от
     val gross: Boolean?, // до или после вычета
-    val to: Int? //сумма до
+    val to: Int? // сумма до
 )
 
 data class LogoUrls(
-    @SerializedName("90") val small: String, //Маленькая иконка
-    @SerializedName("240") val medium: String, //Большая иконка
-    val original: String //стандартная иконка
+    @SerializedName("90") val small: String, // Маленькая иконка
+    @SerializedName("240") val medium: String, // Большая иконка
+    val original: String // стандартная иконка
 )
 
 data class Experience(
@@ -46,11 +46,11 @@ data class KeySkill(
 
 data class Language(
     val name: String, // Название языка
-    val level: LanguageLevel //уровень языка
+    val level: LanguageLevel // уровень языка
 )
 
 data class LanguageLevel(
-    val name: String //Уровень языка
+    val name: String // Уровень языка
 )
 
 data class DriverLicense(
