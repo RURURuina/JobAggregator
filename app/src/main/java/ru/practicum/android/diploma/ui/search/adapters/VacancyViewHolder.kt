b527d660f059cfd9c7a07356e18f0c5ab98ab666
Corpseofpig <Corpseofpig@mail.ru>
2024-11-14 16:05:22 +0300

    Реализовал Форматирование валют и разрядов числа в валюты

--- app/src/main/java/ru/practicum/android/diploma/ui/search/adapters/VacancyViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/adapters/VacancyViewHolder.kt
@@ -1,23 +1,57 @@
 package ru.practicum.android.diploma.ui.search.adapters
 
+import android.icu.text.DecimalFormat
+import android.icu.text.DecimalFormatSymbols
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import ru.practicum.android.diploma.databinding.JobItemBinding
 import ru.practicum.android.diploma.domain.models.entity.Vacancy
+import java.util.Locale
 
 class VacancyViewHolder(private val binding: JobItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    private val currencySymbols = mapOf(
+        "RUR" to "₽",    // Russian Ruble
+        "BYR" to "Br",   // Belarusian Ruble
+        "BYN" to "Br",   // New Belarusian Ruble
+        "USD" to "$",    // US Dollar
+        "EUR" to "€",    // Euro
+        "KZT" to "₸",    // Kazakhstani Tenge
+        "UZS" to "so'm", // Uzbekistani So'm
+        "CNY" to "¥",    // Chinese Yuan
+        "GBP" to "£",    // British Pound
+        "JPY" to "¥",    // Japanese Yen
+        "TRY" to "₺",    // Turkish Lira
+        "INR" to "₹",    // Indian Rupee
+        "AED" to "د.إ",  // UAE Dirham
+        "CAD" to "$",    // Canadian Dollar
+        "AUD" to "$",    // Australian Dollar
+        "CHF" to "₣",    // Swiss Franc
+        "PLN" to "zł",   // Polish Zloty
+    )
+
+    private val numberFormat = DecimalFormat("#,###").apply {
+        decimalFormatSymbols = DecimalFormatSymbols(Locale.getDefault()).apply {
+            groupingSeparator = ' '
+        }
+    }
+
     fun bind(vacancy: Vacancy) {
         binding.apply {
             cardTitleText.text = vacancy.name
             cardDescriptionText.text = vacancy.employer?.name
 
+            val currencySymbol = currencySymbols[vacancy.salary?.currency] ?: vacancy.salary?.currency.orEmpty()
+            val formattedFrom = vacancy.salary?.from?.let { numberFormat.format(it) }
+            val formattedTo = vacancy.salary?.to?.let { numberFormat.format(it) }
+
             val salaryString = when {
-                vacancy.salary?.from != null && vacancy.salary.to != null ->
-                    "От ${vacancy.salary.from} до ${vacancy.salary.to} ${vacancy.salary.currency}"
-                vacancy.salary?.from != null ->
-                    "От ${vacancy.salary.from} ${vacancy.salary.currency}"
-                vacancy.salary?.to != null ->
-                    "До ${vacancy.salary.to} ${vacancy.salary.currency}"
+                formattedFrom != null && formattedTo != null ->
+                    "От $formattedFrom до $formattedTo $currencySymbol"
+                formattedFrom != null ->
+                    "От $formattedFrom $currencySymbol"
+                formattedTo != null ->
+                    "До $formattedTo $currencySymbol"
                 else -> "Зарплата не указана"
             }
 
@@ -31,4 +65,5 @@ class VacancyViewHolder(private val binding: JobItemBinding) : RecyclerView.View
             }
         }
     }
+
 }
