6f7d95f139a7122abf500754366b6963a96f585a
I1kham <offgenius@gmail.com>
2024-11-14 22:48:02 +0300

    bot detekt fix

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filtration/FiltrationViewModel.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.presentation.filtration
+
+import androidx.lifecycle.ViewModel
+
+class FiltrationViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
