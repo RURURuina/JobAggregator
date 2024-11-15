bd92b57d234593d87bb15912faa7b43a421ee524
I1kham <offgenius@gmail.com>
2024-11-15 23:02:32 +0300

    фикс с цветом кнопки фильтра на экране поиска

--- app/src/main/java/ru/practicum/android/diploma/util/Debounce.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/Debounce.kt
@@ -5,10 +5,23 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-fun <T> debounce (delayMillis: Long,
-                  coroutineScope: CoroutineScope,
-                  useLastParam: Boolean,
-                  action: (T) -> Unit): (T) -> Unit {
+/*предлогаю пользовать эту функцию, она не блокирует поток
+Функция debounce используется для отложенного выполнения действия.
+ Она принимает три параметра: время задержки в миллисекундах, объект
+ CoroutineScope и логический параметр useLastParam. Если useLastParam
+  установлен в true, то предыдущая отложенная задача будет отменена перед
+   запуском новой. Внутри функции создается объект Job, который отвечает
+   за отложенное выполнение действия. Если useLastParam установлен в true
+    или предыдущая отложенная задача уже завершена, то новая отложенная задача
+    запускается с помощью функции launch. Внутри отложенной задачи используется
+    функция delay для задержки выполнения на указанное время, а затем выполняется
+    действие с помощью переданной функции action. */
+fun <T> debounce(
+    delayMillis: Long,
+    coroutineScope: CoroutineScope,
+    useLastParam: Boolean,
+    action: (T) -> Unit,
+): (T) -> Unit {
     var debounceJob: Job? = null
     return { param: T ->
         if (useLastParam) {
