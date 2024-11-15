3f37b864a27ff6ba609f95513ae7b95d0d2fabec
Ilkham <offgenius@gmail.com>
2024-11-15 11:52:21 +0300

    auth problem resolving

--- app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/VacancyDetailsViewModel.kt
+++
@@ -1,7 +0,0 @@
-package ru.practicum.android.diploma.presentation.vacancy
-
-import androidx.lifecycle.ViewModel
-
-class VacancyDetailsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
