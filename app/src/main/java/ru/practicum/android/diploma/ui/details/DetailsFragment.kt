71a2113101b0ead20a39f2f381c31b7b6a81c6ad
jimma <rumuzi1991@gmail.com>
2024-11-15 10:55:55 +0300

    Создание верстки экрана деталей вакансий. Исправления детект

--- app/src/main/java/ru/practicum/android/diploma/ui/details/DetailsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/details/DetailsFragment.kt
@@ -10,12 +10,11 @@ import ru.practicum.android.diploma.R
 class DetailsFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_details, container, false)
     }
-
-
 }
