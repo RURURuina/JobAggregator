d9d4ffd2d85f216b30c65c9a7c778a47fb97e8aa
Anton <boroda_c_m9com@mail.ru>
2024-11-16 23:33:31 +0300

    добавил прогресс-бар в разметку(на всякий случай), стейт экрана Избранные, заполнил Фрагмент и ВьюМодел

--- app/src/main/java/ru/practicum/android/diploma/presentation/favorites/FavoriteJobViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/favorites/FavoriteJobViewModel.kt
@@ -1,9 +1,23 @@
 package ru.practicum.android.diploma.presentation.favorites
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.practicum.android.diploma.ui.favorites.models.FavoritesState
 
 class FavoriteJobViewModel : ViewModel() {
     init {
-        println("FavoriteJobViewModel_startUp")
+        // тут пока пусто
+    }
+
+    // liveData для отрисовки экрана на будущее
+    private val _favoritesState = MutableLiveData<FavoritesState>()
+    val favoritesState: LiveData<FavoritesState> = _favoritesState
+
+
+
+    //функция для обновления liveData
+    private fun pushFavoriteState(state: FavoritesState){
+        _favoritesState.postValue(state)
     }
 }
