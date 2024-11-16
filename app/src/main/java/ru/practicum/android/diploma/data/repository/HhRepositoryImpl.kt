ad0f02d219aba0f609f1d92c39027387c9abbfc3
I1kham <offgenius@gmail.com>
2024-11-16 17:28:19 +0300

    переписал на сеалд класс

--- app/src/main/java/ru/practicum/android/diploma/data/repository/HhRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/repository/HhRepositoryImpl.kt
@@ -18,14 +18,6 @@ class HhRepositoryImpl(
     private val vacancyDtoConvertor: VacancyDtoConvertor,
 ) : HhRepository {
 
-    companion object{
-        private const val NO_INTERNET_CODE =-1
-        private const val SUCCESS_CODE =200
-        private const val MIN_SERVER_ERROR_CODE = 500
-        private const val MAX_SERVER_ERROR_CODE = 599
-
-    }
-
     override suspend fun getVacancies(expression: HashMap<String, String>): Flow<Resource<List<Vacancy>>> = flow {
         val response = networkClient.getVacancies(VacanciesSearchRequest(expression))
 
