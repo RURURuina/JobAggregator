90dfbc7d712b09ba12229d3519d1556a338cbade
Anton <boroda_c_m9com@mail.ru>
2024-11-18 16:23:08 +0300

    причесал немного еще раз, чтобы бот не ругался

--- app/src/main/java/ru/practicum/android/diploma/ui/favorites/models/FavoritesState.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/favorites/models/FavoritesState.kt
@@ -3,18 +3,18 @@ package ru.practicum.android.diploma.ui.favorites.models
 import ru.practicum.android.diploma.domain.models.entity.Vacancy
 
 sealed interface FavoritesState {
-    data object Loading: FavoritesState
+    data object Loading : FavoritesState
 
     data class Content(
         val favoritesVacancies: List<Vacancy>
-    ): FavoritesState
+    ) : FavoritesState
 
     data class Empty(
-       val message: Int
-    ): FavoritesState
+        val message: Int
+    ) : FavoritesState
 
     data class Error(
-       val message: Int
-    ): FavoritesState
+        val message: Int
+    ) : FavoritesState
 
 }
