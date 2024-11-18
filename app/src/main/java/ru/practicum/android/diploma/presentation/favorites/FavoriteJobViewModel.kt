6d4078958eb644b62aeb668cab859667ebb5ba90
Anton <boroda_c_m9com@mail.ru>
2024-11-18 13:56:10 +0300

    доработал фрагмент и вьюМодл, добавил обработку ошибок получения данных из бд

--- app/src/main/java/ru/practicum/android/diploma/presentation/favorites/FavoriteJobViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/favorites/FavoriteJobViewModel.kt
@@ -3,9 +3,16 @@ package ru.practicum.android.diploma.presentation.favorites
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.domain.models.entity.Vacancy
 import ru.practicum.android.diploma.ui.favorites.models.FavoritesState
 
-class FavoriteJobViewModel : ViewModel() {
+class FavoriteJobViewModel(
+//   нужно добавить интерактор
+//    private val interactor: FavoriteJobInteractor
+    ) : ViewModel() {
     init {
         // тут пока пусто
     }
@@ -14,7 +21,28 @@ class FavoriteJobViewModel : ViewModel() {
     private val _favoritesState = MutableLiveData<FavoritesState>()
     val favoritesState: LiveData<FavoritesState> = _favoritesState
 
+    fun getVacancies(){
+        viewModelScope.launch {
+            /*
+            Блок для получения вакансий Например:
+            interactor.getVacancies()
+                .onStart { pushFavoriteState(FavoriteState.Loading)}
+                .catch {exception->
+                    pushFavoriteState(FavoriteState.Error(R.string.couldnt_get_job_list))    //обработка ошибок
+                .collect { vacancies->
+                    showData(vacancies)
+             */
+        }
+    }
 
+    private fun showData(vacancies: List<Vacancy>){
+
+        if (vacancies.isEmpty()){
+            pushFavoriteState(FavoritesState.Empty(R.string.empty_list))
+        }else{
+            pushFavoriteState(FavoritesState.Content(vacancies))
+        }
+    }
 
     //функция для обновления liveData
     private fun pushFavoriteState(state: FavoritesState){
