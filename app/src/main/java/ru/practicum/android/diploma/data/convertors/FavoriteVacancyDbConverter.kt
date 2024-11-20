7ee069911fe81dce9ab762f5842bc8e3441ea898
Anton <boroda_c_m9com@mail.ru>
2024-11-20 23:04:10 +0300

    решил детект с потерей ошибки и слишком нагруженным методом FavoriteVacancyDbConverter логированием b ltrjvgjpbwbtq cjjndtncndtyyj

--- app/src/main/java/ru/practicum/android/diploma/data/convertors/FavoriteVacancyDbConverter.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/convertors/FavoriteVacancyDbConverter.kt
@@ -1,5 +1,6 @@
 package ru.practicum.android.diploma.data.convertors
 
+import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
 import com.google.gson.reflect.TypeToken
@@ -44,56 +45,126 @@ class FavoriteVacancyDbConverter(
     )
 
     fun vacancyEntityToDomain(vacancyEntity: FavoritesVacancyEntity): Vacancy {
-        fun <T> safeFromJson(json: String?, typeOfT: TypeToken<T>): T? {
-            return try {
-                gson.fromJson(json, typeOfT.type)
-            } catch (e: JsonSyntaxException) {
-                null
-            }
-        }
         return Vacancy(
             id = vacancyEntity.id,
             name = vacancyEntity.name,
-            employer = safeFromJson(vacancyEntity.employer, object : TypeToken<Employer>() {}) ?: Employer(null, null),
-            salary = safeFromJson(vacancyEntity.salary, object : TypeToken<Salary>() {}) ?: Salary(
-                null,
-                null,
-                null,
-                null
-            ),
-            adress = safeFromJson(vacancyEntity.address, object : TypeToken<Address>() {}) ?: Address(
-                null,
-                null,
-                null,
-                null,
-                null,
-                null
-            ),
-            experience = safeFromJson(vacancyEntity.experience, object : TypeToken<Experience>() {})
-                ?: Experience(null),
-            employment = safeFromJson(vacancyEntity.employment, object : TypeToken<Employment>() {})
-                ?: Employment(null),
-            keySkills = safeFromJson(vacancyEntity.keySkills, object : TypeToken<List<KeySkill>>() {}) ?: emptyList(),
-            languages = safeFromJson(vacancyEntity.languages, object : TypeToken<List<Language>>() {}) ?: emptyList(),
-            driverLicenseTypes = safeFromJson(
-                vacancyEntity.driverLicenseTypes,
-                object : TypeToken<List<DriverLicense>>() {}) ?: emptyList(),
-            area = safeFromJson(vacancyEntity.area, object : TypeToken<Area>() {}) ?: Area(null, null, null),
-            industry = safeFromJson(vacancyEntity.industry, object : TypeToken<Industry>() {}) ?: Industry(
-                "",
-                "",
-                null
-            ),
-            country = safeFromJson(vacancyEntity.country, object : TypeToken<Country>() {}) ?: Country("", ""),
-            contacts = safeFromJson(vacancyEntity.contacts, object : TypeToken<Contacts>() {}) ?: Contacts(
-                null,
-                null,
-                null
-            ),
+            employer = getEmployer(vacancyEntity),
+            salary = getSalary(vacancyEntity),
+            adress = getAddress(vacancyEntity),
+            experience = getExperience(vacancyEntity),
+            employment = getEmployment(vacancyEntity),
+            keySkills = getKeySkills(vacancyEntity),
+            languages = getLanguages(vacancyEntity),
+            driverLicenseTypes = getDriverLicenseTypes(vacancyEntity),
+            area = getArea(vacancyEntity),
+            industry = getIndustry(vacancyEntity),
+            country = getCountry(vacancyEntity),
+            contacts = getContacts(vacancyEntity),
             description = vacancyEntity.description,
-            schedule = safeFromJson(vacancyEntity.schedule, object : TypeToken<Schedule>() {}) ?: Schedule(null, null),
+            schedule = getSchedule(vacancyEntity),
             url = vacancyEntity.url,
             isFavorite = true
         )
+    }
+
+    private fun getEmployer(vacancyEntity: FavoritesVacancyEntity): Employer {
+        return safeFromJson(vacancyEntity.employer, object : TypeToken<Employer>() {}) ?: Employer(
+            null,
+            null
+        )
+    }
+
+
+    private fun getSalary(vacancyEntity: FavoritesVacancyEntity): Salary {
+        return safeFromJson(vacancyEntity.salary, object : TypeToken<Salary>() {}) ?: Salary(
+            null,
+            null,
+            null,
+            null
+        )
+    }
+
+    private fun getAddress(vacancyEntity: FavoritesVacancyEntity): Address {
+        return safeFromJson(vacancyEntity.address, object : TypeToken<Address>() {}) ?: Address(
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        )
+    }
+
+    private fun getExperience(vacancyEntity: FavoritesVacancyEntity): Experience {
+        return safeFromJson(vacancyEntity.experience, object : TypeToken<Experience>() {})
+            ?: Experience(null)
+    }
+
+    private fun getEmployment(vacancyEntity: FavoritesVacancyEntity): Employment {
+        return safeFromJson(vacancyEntity.employment, object : TypeToken<Employment>() {})
+            ?: Employment(null)
+    }
+
+    private fun getKeySkills(vacancyEntity: FavoritesVacancyEntity): List<KeySkill> {
+        return safeFromJson(vacancyEntity.keySkills, object : TypeToken<List<KeySkill>>() {})
+            ?: emptyList()
+    }
+
+    private fun getLanguages(vacancyEntity: FavoritesVacancyEntity): List<Language> {
+        return safeFromJson(vacancyEntity.languages, object : TypeToken<List<Language>>() {})
+            ?: emptyList()
+    }
+
+    private fun getDriverLicenseTypes(vacancyEntity: FavoritesVacancyEntity): List<DriverLicense> {
+        return safeFromJson(
+            vacancyEntity.driverLicenseTypes,
+            object : TypeToken<List<DriverLicense>>() {}) ?: emptyList()
+    }
+
+    private fun getArea(vacancyEntity: FavoritesVacancyEntity): Area {
+        return safeFromJson(vacancyEntity.area, object : TypeToken<Area>() {}) ?: Area(
+            null,
+            null,
+            null
+        )
+    }
+
+    private fun getIndustry(vacancyEntity: FavoritesVacancyEntity): Industry {
+        return safeFromJson(vacancyEntity.industry, object : TypeToken<Industry>() {}) ?: Industry(
+            "",
+            "",
+            null
+        )
+    }
+
+    private fun getCountry(vacancyEntity: FavoritesVacancyEntity): Country {
+        return safeFromJson(vacancyEntity.country, object : TypeToken<Country>() {}) ?: Country(
+            "",
+            ""
+        )
+    }
+
+    private fun getContacts(vacancyEntity: FavoritesVacancyEntity): Contacts {
+        return safeFromJson(vacancyEntity.contacts, object : TypeToken<Contacts>() {}) ?: Contacts(
+            null,
+            null,
+            null
+        )
+    }
+
+    private fun getSchedule(vacancyEntity: FavoritesVacancyEntity): Schedule {
+        return safeFromJson(vacancyEntity.schedule, object : TypeToken<Schedule>() {}) ?: Schedule(
+            null,
+            null
+        )
+    }
+
+    private fun <T> safeFromJson(json: String?, typeOfT: TypeToken<T>): T? {
+        return try {
+            gson.fromJson(json, typeOfT.type)
+        } catch (e: JsonSyntaxException) {
+            Log.e("FavoriteVacancyDbConverter", "JsonSyntaxException, $e")
+            null
+        }
     }
 }
