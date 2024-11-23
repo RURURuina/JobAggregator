7d1d71626a23d5778391cddede408aee5f42c788
Corpseofpig <Corpseofpig@mail.ru>
2024-11-23 14:36:46 +0300

    Создана viewModel

--- app/src/main/java/ru/practicum/android/diploma/presentation/industry/IndustryViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/industry/IndustryViewModel.kt
@@ -1,7 +1,44 @@
 package ru.practicum.android.diploma.presentation.industry
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.domain.api.industries.IndustriesInteractor
+import ru.practicum.android.diploma.domain.models.entity.Industry
+import ru.practicum.android.diploma.domain.models.entity.IndustryNested
+import ru.practicum.android.diploma.util.Resource
 
-class IndustryViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class IndustryViewModel(val interactor: IndustriesInteractor) : ViewModel() {
+    private var unFilteredList : List<IndustryNested> = emptyList()
+
+     private val _industries = MutableLiveData<List<IndustryNested>>()
+     var industries: LiveData<List<IndustryNested>> = _industries
+
+    private val _selectedIndustry = MutableLiveData<IndustryNested>()
+    val selectedIndustry: LiveData<IndustryNested> = _selectedIndustry
+
+    init {
+        getIndustries()
+    }
+
+    private fun getIndustries() {
+        viewModelScope.launch {
+            interactor.getIndustriesList().collect { result ->
+                when(result) {
+                    is Resource.Success -> {
+                      unFilteredList = result.data?.flatMap { industry ->
+                          industry.industries!!
+                      }!!
+                        _industries.value = unFilteredList
+                    }
+
+                    is Resource.Error -> {
+                        //обработка ошибки
+                    }
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/filtration/FiltrationFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filtration/FiltrationFragment.kt
@@ -6,27 +6,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.presentation.filtration.FiltrationViewModel
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentFiltrationBinding
 
 class FiltrationFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = FiltrationFragment()
-    }
-
     private val viewModel: FiltrationViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    private var binding: FragmentFiltrationBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View {
-        return inflater.inflate(R.layout.fragment_filtration, container, false)
+    ): View? {
+        binding = FragmentFiltrationBinding.inflate(layoutInflater)
+        return binding?.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupButtons()
+    }
+
+    // настройка листениров кнопок
+   fun setupButtons() {
+       binding?.indistryBtn?.setOnClickListener {
+           findNavController().navigate(R.id.action_filtrationFragment_to_industryFragment)
+       }
+   }
 }
