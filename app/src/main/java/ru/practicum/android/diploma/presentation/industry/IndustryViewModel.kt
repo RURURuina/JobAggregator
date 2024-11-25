5094528e503e744e994c76d213ea3e18cf8cd930
I1kham <offgenius@gmail.com>
2024-11-25 20:18:04 +0300

    инит ветки

--- app/src/main/java/ru/practicum/android/diploma/presentation/industry/IndustryViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/industry/IndustryViewModel.kt
@@ -1,9 +1,18 @@
 package ru.practicum.android.diploma.presentation.industry
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.domain.api.industries.IndustriesInteractor
 
-class IndustryViewModel : ViewModel() {
+class IndustryViewModel(val industriesInteractor: IndustriesInteractor) : ViewModel() {
     init {
-        println("IndustryViewModel init")
+        viewModelScope.launch {
+            industriesInteractor.getIndustriesList().collect{result->
+               val a= result.data?.let {
+
+               }
+            }
+        }
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/industry/IndustryFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/industry/IndustryFragment.kt
@@ -6,22 +6,31 @@ import android.view.ViewGroup
 import androidx.core.bundle.Bundle
 import androidx.fragment.app.Fragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentIndustryBinding
 import ru.practicum.android.diploma.presentation.industry.IndustryViewModel
 
 class IndustryFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = IndustryFragment()
-    }
-
     private val viewModel: IndustryViewModel by viewModel()
+    private var _binding: FragmentIndustryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        return inflater.inflate(R.layout.fragment_industry, container, false)
+        _binding = FragmentIndustryBinding.inflate(layoutInflater)
+        return this.binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: android.os.Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        println()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        _binding = null
     }
 }
