f1d0591f7be4485b47507b422add5eac39f8495d
Ilkham <offgenius@gmail.com>
2024-11-26 09:50:30 +0300

    устранение детект

--- app/src/main/java/ru/practicum/android/diploma/data/convertors/SharedStringConvertor.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/convertors/SharedStringConvertor.kt
@@ -1,7 +1,5 @@
 package ru.practicum.android.diploma.data.convertors
 
-
-
 import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
--- app/src/main/java/ru/practicum/android/diploma/domain/models/entity/FilterShared.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/entity/FilterShared.kt
@@ -9,16 +9,4 @@ data class FilterShared(
     val industryId: String?,
     val salary: String?,
     val onlySalaryFlag: Boolean?
-) {
-    fun FilterShared.getResponseWithFilters(text: String, page: Int?, pageSize: Int?): HashMap<String, String> {
-        return hashMapOf(
-            "text" to text,
-            "page" to page.toString(),
-            "per_page" to pageSize.toString(),
-            "area" to this.regionId.toString(),
-            "industry" to this.industryId.toString(),
-            "salary" to this.salary.toString(),
-            "only_with_salary" to this.onlySalaryFlag.toString()
-        )
-    }
-}
+)
