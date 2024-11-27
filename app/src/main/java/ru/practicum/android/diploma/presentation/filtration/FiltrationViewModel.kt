2c634d476fae214e8ab16a522c562f934932c070
jimma <rumuzi1991@gmail.com>
2024-11-27 11:47:17 +0300

    viewModel экрана фильтр

--- app/src/main/java/ru/practicum/android/diploma/presentation/filtration/FiltrationViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filtration/FiltrationViewModel.kt
@@ -1,9 +1,45 @@
 package ru.practicum.android.diploma.presentation.filtration
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.domain.api.filter.FilterInteractor
+import ru.practicum.android.diploma.domain.models.entity.FilterShared
+import ru.practicum.android.diploma.domain.models.entity.Salary
 
-class FiltrationViewModel : ViewModel() {
-    init {
-        println("FiltrationViewModel init")
+class FiltrationViewModel(private val filterInteractor: FilterInteractor) : ViewModel() {
+    private val _filterState = MutableLiveData<FilterShared?>()
+    val filterState : LiveData<FilterShared?> = _filterState
+
+    fun loadSavedFilter() {
+        viewModelScope.launch {
+            _filterState.value = filterInteractor.getFilter()
+        }
+    }
+
+    fun saveFilter(filterShared: FilterShared) {
+        viewModelScope.launch {
+            filterInteractor.saveFilter(filterShared)
+            _filterState.value = filterShared
+        }
+    }
+
+    fun createFilterFromUI(salary: String?, onlySalaryFlag: Boolean?) : FilterShared {
+        val currentFilter = _filterState.value ?: FilterShared(
+            countryName = null,
+            countryId = null,
+            regionName = null,
+            regionId = null,
+            industryName = null,
+            industryId = null,
+            salary = null,
+            onlySalaryFlag = null
+        )
+        return currentFilter.copy(
+            salary = salary,
+            onlySalaryFlag = onlySalaryFlag
+        )
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/filtration/FiltrationFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filtration/FiltrationFragment.kt
@@ -1,9 +1,10 @@
 package ru.practicum.android.diploma.ui.filtration
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.bundle.Bundle
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -26,31 +27,87 @@ class FiltrationFragment : Fragment() {
         return this.binding.root
     }
 
-    override fun onDetach() {
-        super.onDetach()
-        navBarVisible(true)
-        _binding = null
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: android.os.Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navBarVisible(false)
-        prepareRegionButton()
-        prepareIndustryButton()
+
+        // Загрузка сохраненного фильтра
+        viewModel.loadSavedFilter()
+
+        setupObservers()
+        setupListeners()
+        prepareButtons()
+
     }
 
-    private fun prepareIndustryButton() {
-        binding.industryLayout.setOnClickListener {
-            findNavController().navigate(R.id.action_filtrationFragment_to_industryFragment)
+    private fun setupObservers() {
+        viewModel.filterState.observe(viewLifecycleOwner) { filter ->
+            filter?.let {
+                // Заполнение полей сохраненными значениями
+                if(it.regionName != null && it.countryName != null)  {
+                    binding.regionText.text = it.countryName + "," + it.regionName
+                }
+                binding.industryText.setText(it.industryName ?: "")
+                binding.inputSalary.setText(it.salary ?: "")
+                binding.checkBox.isChecked = it.onlySalaryFlag ?: false
+            }
+            setApplyResetButtonsVis(false)
         }
     }
 
-    private fun prepareRegionButton() {
+    private fun setApplyResetButtonsVis(vis: Boolean){
+        if(vis){
+            binding.applyButton.visibility = View.VISIBLE
+            binding.resetText.visibility = View.VISIBLE
+        } else{
+            binding.applyButton.visibility = View.GONE
+            binding.resetText.visibility = View.GONE
+        }
+    }
+
+    private fun setupListeners() {
+        // Слушатель изменения зарплаты
+        binding.inputSalary.doAfterTextChanged { text ->
+            setApplyResetButtonsVis(true)
+        }
+
+        // Слушатель чекбокса
+        binding.checkBox.setOnCheckedChangeListener { _, isChecked ->
+            setApplyResetButtonsVis(true)
+        }
+
+        // Кнопка назад
+        binding.backBtn.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        //Кнопка применить
+        binding.applyButton.setOnClickListener{
+            val filter = viewModel.createFilterFromUI(
+                salary = binding.inputSalary.text.toString(),
+                onlySalaryFlag = binding.checkBox.isChecked
+            )
+            viewModel.saveFilter(filter)
+        }
+
+        //Кнопка сбросить
+        binding.resetText.setOnClickListener{
+            val filter = viewModel.createFilterFromUI(
+                salary = "",
+                onlySalaryFlag = false
+            )
+            viewModel.saveFilter(filter)
+            viewModel.loadSavedFilter()
+        }
+    }
+
+    private fun prepareButtons() {
+        // Кнопки выбора региона и отрасли
         binding.workPlaceLayout.setOnClickListener {
             findNavController().navigate(R.id.action_filtrationFragment_to_selectRegionFragment)
         }
 
-        binding.industryBtn.setOnClickListener {
+        binding.industryLayout.setOnClickListener {
             findNavController().navigate(R.id.action_filtrationFragment_to_industryFragment)
         }
     }
@@ -59,4 +116,9 @@ class FiltrationFragment : Fragment() {
         (activity as RootActivity).bottomNavigationVisibility(isVisible)
     }
 
+    override fun onDetach() {
+        super.onDetach()
+        navBarVisible(true)
+        _binding = null
+    }
 }
