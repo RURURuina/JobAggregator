e431675a47dc4cde92997ad221079df269838f69
Ilkham <offgenius@gmail.com>
2024-12-03 12:08:03 +0300

    детект

--- app/src/main/java/ru/practicum/android/diploma/util/ImageViewFiller.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/ImageViewFiller.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.target.ViewTarget
 import ru.practicum.android.diploma.R
 
